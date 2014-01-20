a77119f4308741a3be2c68470d483cc331866ab5
Andreas Bergmeier <abergmeier@gmx.net>
2014-01-20 10:47:42 +0100

    Make destructor of ofGLProgrammableRenderer noexcept.

--- libs/openFrameworks/gl/ofGLProgrammableRenderer.h
+++ libs/openFrameworks/gl/ofGLProgrammableRenderer.h
@@ -16,7 +16,7 @@ class ofVbo;
 class ofGLProgrammableRenderer: public ofBaseGLRenderer{
 public:
 	ofGLProgrammableRenderer(bool useShapeColor=true);
-	~ofGLProgrammableRenderer();
+	~ofGLProgrammableRenderer() noexcept = default;
 
 	void setup();
 
